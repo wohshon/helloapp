c9c82ac16d76872ae22978953f5a511b490cc4f5
ws <wohshon@gmail.com>
2019-08-22 16:16:23 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+//kc
+    var session = require('express-session');
+    var Keycloak = require('keycloak-connect');
+
+    var memoryStore = new session.MemoryStore();
+    var keycloak = new Keycloak({ store: memoryStore });
+    app.use( keycloak.middleware() );
+
+
+app.use('/',keycloak.protect(), indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
