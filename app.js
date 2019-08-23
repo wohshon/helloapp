9e43a57bb419c86bf2bab81ff1ad3d6fa43ec1f0
ws <wohshon@gmail.com>
2019-08-23 10:58:26 +0800

    update title

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -38,7 +38,8 @@ app.use(session(sess));
 app.use( keycloak.middleware() );
 
 
-app.use('/',keycloak.protect(), indexRouter);
+//app.use('/',keycloak.protect(), indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
